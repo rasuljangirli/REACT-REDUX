c7b03df9a1f718c71b33d7f45d167a24462f842d
rasuljangirli <127443671+rasuljangirli@users.noreply.github.com>
2024-07-11 17:29:11 +0400

    Update store.jsx

--- 12.Redux Toolkit HTTP sorgusu/src/redux/store.jsx
+++ 12.Redux Toolkit HTTP sorgusu/src/redux/store.jsx
@@ -3,6 +3,7 @@ import counterReducer from "./counterSlice";
 import userReducer from "./usersSlice";
 
 export const store = configureStore({
+  
   reducer: {
     counter: counterReducer,
     users: userReducer,
